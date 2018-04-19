183e72e324818c7ea4fca797e109b23c603dda08
weishuwen <870224929@qq.com>
2018-04-19 11:22:09 +0800

    增加wiremock

---
+++ demo/src/main/java/com/wsw/web/wiremock/MockServer.java
@@ -0,0 +1,25 @@
+package com.wsw.web.wiremock;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.io.FileUtils;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+public class MockServer {
+
+    public static void main(String[] args) throws IOException {
+        configureFor(9999);
+        removeAllMappings();
+
+        mock("/order/1", "01");
+    }
+
+    private static void mock(String url, String file) throws IOException {
+        ClassPathResource resource = new ClassPathResource("mock/response/" + file + ".txt");
+        String content = StringUtils.join(FileUtils.readLines(resource.getFile(), "UTF-8").toArray(), "\n");
+        stubFor(get(urlPathEqualTo(url)).willReturn(aResponse().withBody(content).withStatus(200)));
+    }
+}
